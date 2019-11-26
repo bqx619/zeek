b1e5fe6b4028200ddb0546d24ea863f4c6ab5c35
Tim Wojtulewicz <tim@corelight.com>
2019-11-26 12:59:28 -0700

    Broker manager changes to match the new IOSource API and loop architecture

--- src/broker/Manager.h
+++ src/broker/Manager.h
@@ -346,15 +346,9 @@ private:
 		__attribute__((format (printf, 2, 3)));
 
 	// IOSource interface overrides:
-	void GetFds(iosource::FD_Set* read, iosource::FD_Set* write,
-	            iosource::FD_Set* except) override;
-
-	double NextTimestamp(double* local_network_time) override;
-
 	void Process() override;
-
-	const char* Tag() override
-		{ return "Broker::Manager"; }
+	const char* Tag() override	{ return "Broker::Manager"; }
+	double GetNextTimeout() override	{ return -1; }
 
 	struct LogBuffer {
 		// Indexed by topic string.
